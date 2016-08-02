64abc6df4155124367cccb972c8099a247875f6a
tbalden <illespal@gmail.com>
2016-08-02 14:37:31 -0700

    gpu: devfreq: adrenoboost: adding conservative governor
    Adding a governor where moving upwards at lowest levels
    and highest levels is hard (defending 2d and overboost),
    and easy to change freq level in the middle part of the
    frequency levels.
    
    Conflicts:
    	drivers/devfreq/governor_msm_adreno_tz.c
    
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>
    Signed-off-by: cbendot <r.budhiono@gmail.com>

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -397,21 +397,8 @@ extern int simple_gpu_algorithm(int level, int *val,
 #if 1
 
 // mapping gpu level calculated linear conservation half curve values into a
-// bell curve of conservation  (lower is higher freq level)
-static int conservation_map_up[] = {15,15,10,4,5,6,12     ,5,5,5};
-static int conservation_map_down[] = {0,1,6,6,5,0,0     ,5,5,5};
-
-// make boost multiplication/division depending on current lvl, dampen the high freq up scaling! (lower is higher freq level)
-static int lvl_multiplicator_map_1[] = {5,5,6,8,9,1,1    ,1,1};
-static int lvl_divider_map_1[] = {10,10,10,10,10,1,1    ,1,1};
-
-// for boost == 2 -- boost divide on the low spectrum, dampen the lower freq values, unneeded to boost the low freq spectrum so much at start
-static int lvl_multiplicator_map_2[] = {9,1,1,1,1,10,8    ,1,1};
-static int lvl_divider_map_2[] = {10,1,1,1,1,14,12    ,1,1};
-
-// for boost == 3 -- boost divide on the low spectrum, dampen the lower freq values, unneeded to boost the low freq spectrum so much at start
-static int lvl_multiplicator_map_3[] = {10,1,1,1,1,11,9    ,1,1};
-static int lvl_divider_map_3[] = {10,1,1,1,1,15,13    ,1,1};
+// half bell curve of conservation
+static int conservation_map[] = {0,1,2,5,6,9,14     ,17,20,25};
 
 #endif
 
@@ -453,14 +440,15 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 	priv->bin.total_time += stats.total_time;
 #if 1
 	// scale busy time up based on adrenoboost parameter, only if MIN_BUSY exceeded...
-	if ((unsigned int)(priv->bin.busy_time + stats.busy_time) >= MIN_BUSY && adrenoboost) {
-		if (adrenoboost == 1) {
-			priv->bin.busy_time += (unsigned int)((stats.busy_time * ( 1 + adrenoboost ) * lvl_multiplicator_map_1[ last_level ]) / lvl_divider_map_1[ last_level ]);
+
+	if ((unsigned int)(priv->bin.busy_time + stats.busy_time) >= MIN_BUSY) {
+		if (adrenoboost <= 1) {
+			priv->bin.busy_time += stats.busy_time * ( 1 + adrenoboost );
 		} else
 		if (adrenoboost == 2) {
-			priv->bin.busy_time += (unsigned int)((stats.busy_time * ( 1 + 3 ) * lvl_multiplicator_map_2[ last_level ]  * 8 ) / (lvl_divider_map_2[ last_level ] * 10));
+			priv->bin.busy_time += (unsigned int)((stats.busy_time * ( 1 + adrenoboost ) * 7)/10);
 		} else {
-			priv->bin.busy_time += (unsigned int)((stats.busy_time * ( 1 + 4 ) * lvl_multiplicator_map_3[ last_level ]  * 9 ) / (lvl_divider_map_3[ last_level ] * 10));
+			priv->bin.busy_time += (unsigned int)((stats.busy_time * ( 1 + adrenoboost ) * 7)/10);
 		}
 	} else {
 		priv->bin.busy_time += stats.busy_time;
@@ -536,10 +524,10 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 		if (val) {
 			priv->bin.cycles_keeping_level += 1 + abs(val/2); // higher value change quantity means more addition to cycles_keeping_level for easier switching
 			// going upwards in frequency -- make it harder on the low and high freqs, middle ground - let it move
-			if (val<0 && priv->bin.cycles_keeping_level < conservation_map_up[ last_level ]) {
+			if (val<0 && priv->bin.cycles_keeping_level < conservation_map[ abs((priv->bin.last_level * 2) - max_state_val) ]) {
 			} else
 			// going downwards in frequency let it happen hard in the middle freqs
-			if (val>0 && priv->bin.cycles_keeping_level < conservation_map_down[ last_level ])  {
+			if (val>0 && priv->bin.cycles_keeping_level < conservation_map[ (max_state_val - abs((priv->bin.last_level * 2) - max_state_val )) ])  {
 			} else
 			{
 				// reset keep cylcles timer
